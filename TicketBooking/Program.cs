a78df06c502c4224576a6835c9a2a7ef75fefe49
Andrey Pukhkalo <puhkaloandrew@gmail.com>
2021-09-13 17:48:58 +0300

    Add project files.

---
+++ TicketBooking/Program.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace TicketBooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movies = File.ReadAllText($"../../../Files/Movies.json");
+
+            var list = JsonConvert.DeserializeObject<List<Movie>>(movies);
+
+            int length = 100;
+
+            var films = new List<Film>();
+
+            for (int i = 0; i < length; i++)
+            {
+                films.Add(new Film() { Name = $"Film {i.ToString("D3")}" });
+            }
+
+            //Console.WriteLine("1. Find by name");
+            //Console.WriteLine("2. Order by name");
+            //Console.WriteLine("3. Book a ticket");
+            //Console.WriteLine("4. Add new film");
+
+
+            WriteMenu(list);
+            //var asd =  Console.ReadLine();
+
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                keyInfo = Console.ReadKey();
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.Backspace:
+
+                        break;
+
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        list = list.OrderBy(i => i.Title).ToList();
+
+                        File.WriteAllText($"../../../Files/Movies.json", JsonConvert.SerializeObject(list));
+
+                        WriteMenu(list);
+
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            while (keyInfo.Key != ConsoleKey.X);
+
+        }
+
+        private static void WriteMenu(List<Movie> list)
+        {
+            Console.Clear();
+
+            int va = list.Max(movie => movie.Title.Length);
+            Console.WriteLine(va);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"| #  | Name{new string(' ', va - 4)} |");
+            Console.WriteLine($"-----------{new string('-', va - 4)}--");
+
+            foreach (var iterator in list.Select((item, index) => (item, index)))
+            {
+                var index = iterator.index;
+                var item = iterator.item;
+
+                Console.WriteLine($" {(index + 1).ToString("D3")} | {item.Title}{new string(' ', va - item.Title.Length)} |");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("1. Find by name | 2. Order by name | 3. Book a ticket | 4. Booking list | 5. Add new film");
+        }
+    }
+
+    internal class Movie
+    {
+        public string Title { get; set; }
+    }
+
+    internal class Film
+    {
+        public string Name { get; internal set; }
+    }
+}
---
+++ WriteAllLinesAsync/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WriteAllLinesAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ReadAllLines.ExampleAsync().GetAwaiter().GetResult();
+            //WriteAllLines.ExampleAsync().GetAwaiter().GetResult();
+        }
+    }
+
+    public class WriteAllLines
+    {
+        public static async Task ExampleAsync()
+        {
+            string[] lines =
+            {
+            "First line", "Second line", "Third line"
+        };
+
+            await File.WriteAllLinesAsync("WriteLines.json", lines);
+        }
+    }
+
+    public class ReadAllLines
+    {
+        public static async Task ExampleAsync()
+        {
+            string[] lines = await File.ReadAllLinesAsync("WriteLines.txt");
+        }
+    }
+}
